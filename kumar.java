b6ab558bbe06bf8b4ea072ae7b8144e25fda0239
Muthukumark98 <muthukumar1998mk98@gmail.com>
2022-03-17 13:09:36 +0530

    updated my code

---
+++ kumar.java
@@ -0,0 +1,18 @@
+public class kumar extends Parent3
+{
+int no;
+public kumar(int no)
+{
+this.no = no;
+System.out.println("CC" + no);
+}
+public kumar()
+{
+System.out.println("Child Constructor");
+}
+	public static void main(String[] args)
+	{
+		kumar ch = new kumar();
+		kumar c2 = new kumar(1);
+	}
+}
