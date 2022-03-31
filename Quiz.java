342ae8ca4e29bd3723b401cea0b85257b3500fe8
Muthukumark98 <muthukumar1998mk98@gmail.com>
2022-03-31 11:50:45 +0530

    updated my code

---
+++ Quiz.java
@@ -0,0 +1,14 @@
+package tamilnadu.chennai;
+
+public class Quiz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 16;
+		int b = 3;
+		b+=++a;
+		System.out.println(b++);
+	}
+
+}
+
