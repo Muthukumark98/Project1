a37dfda57142e875d57372c4c447459dd12d87f2
Muthukumark98 <muthukumar1998mk98@gmail.com>
2022-03-31 12:02:30 +0530

    updated my code

---
+++ SchoolRainIfelse.java
@@ -0,0 +1,25 @@
+package tamilnadu.chennai;
+
+public class SchoolRainIfelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SchoolRainIfelse sr = new SchoolRainIfelse();
+		sr.learn_if();
+	}
+
+	private void learn_if() {
+		// TODO Auto-generated method stub
+		boolean School = true;
+		boolean raining = true;
+		if(School== true && raining == true )
+		{
+		System.out.println("Take Umbrella");
+		}
+		else
+		{
+			System.out.println("take Rest");
+		}
+		}
+
+}
