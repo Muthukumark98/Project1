573a1fb38ec5229c476337331d874e915daa0c59
Muthukumark98 <muthukumar1998mk98@gmail.com>
2022-03-31 11:47:46 +0530

    updated my code

---
+++ IncrementDecrement2.java
@@ -0,0 +1,15 @@
+package tamilnadu.chennai;
+
+public class IncrementDecrement2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int a = 5 , b=10;
+   System.out.println(a++ + b++ + a-- + b-- - a++ - b++);
+	             //  (5   +   10)+(6  + 11) - (5 + 10);
+                  //      (15    +  17      - 15)  = 17
+	
+	} 
+
+} 
+
