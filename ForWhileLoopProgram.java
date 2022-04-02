f7eac81af6b346b157109aa4d292fd722a08e0dc
Muthukumark98 <muthukumar1998mk98@gmail.com>
2022-04-02 17:10:06 +0530

    updated my code

---
+++ ForWhileLoopProgram.java
@@ -0,0 +1,58 @@
+package tamilnadu.chennai;
+
+public class ForWhileLoopProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ForWhileLoopProgram fw = new ForWhileLoopProgram();
+		//fw.forWhile_learning();
+		//fw.forWhile_learning2();
+		//fw.forWhile_learning3();
+		fw.forWhile_learning4();
+		//fw.While_learning();
+	}
+	private void forWhile_learning4() {
+		// TODO Auto-generated method stub
+		for(int count= 1;count<=5;count++)
+		{
+		System.out.println(1);
+		}
+	}
+	private void While_learning() {
+		// TODO Auto-generated method stub
+		int count =1;
+		while(count<=5)
+		{
+			System.out.println(1);
+			count++;
+		}
+	}
+	private void forWhile_learning3() {
+		// TODO Auto-generated method stub
+		int no=1;
+		for(no=1;no<=5;no++)
+		{
+		
+		}
+		System.out.println(no);  // output is 6 
+	}
+	private void forWhile_learning2() {
+		// TODO Auto-generated method stub
+		for(int no=1;no<=5;no++)
+		{
+		
+	    }
+		//System.out.println(no);/// error
+	}
+
+	private void forWhile_learning() {
+		// TODO Auto-generated method stub
+		for(;true;)
+			System.out.println("hi");
+		
+		
+		
+}
+
+}
+
