ad614fa38c65f961d443fcee31b91d61c8f44709
Muthukumark98 <muthukumar1998mk98@gmail.com>
2022-04-02 17:03:20 +0530

    updated my code

---
+++ WhileForprogramming.java
@@ -0,0 +1,86 @@
+package tamilnadu.chennai;
+
+public class WhileForprogramming {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WhileForprogramming wf = new WhileForprogramming();
+		//wf.while_loopLearning();
+		//wf.while_loopLearning2();
+		//wf.while_loopLearning3();
+		//wf.For_loopLearning3();
+		//wf.For_loopLearning4();
+		//wf.For_loopLearning5();
+		wf.For_loopLearning6();
+		//wf.For_loopLearning7();
+	}
+
+	private void For_loopLearning7() {
+		// TODO Auto-generated method stub
+		int no=1;
+		for(;;)
+		{
+			System.out.println(no); // infinity the output
+		}
+	}
+ 
+	private void For_loopLearning6() {
+		// TODO Auto-generated method stub
+		int no =1;
+		for(;;no++)
+		{
+			System.out.println(no);  //integer range infinity
+		}
+	}
+
+	private void For_loopLearning5() {
+		// TODO Auto-generated method stub
+		int no=1;
+		for(;no<=5;no++)
+	   {
+		//System.out.println(no);			
+	   }
+	    System.out.println(no);
+	}
+
+	private void while_loopLearning3() {
+		// TODO Auto-generated method stub
+		int no=1;
+		while(no<=5);
+		System.out.println(no);
+	}
+
+	private void For_loopLearning4() {
+		// TODO Auto-generated method stub
+		int no;
+		for(no=1; no<=5;);
+	    System.out.println(no);
+		
+		
+	}
+
+	private void For_loopLearning3() {
+		// TODO Auto-generated method stub
+		int no;
+		for(no=1;no<=5;no++);
+		System.out.println(no);
+	}
+
+	private void while_loopLearning2() {
+		// TODO Auto-generated method stub
+		int no=1;
+		while(no<=5);
+		System.out.println(no);
+		no++;
+	}
+
+	private void while_loopLearning() {
+		// TODO Auto-generated method stub
+		int no=1;
+		while(no<=5)
+		System.out.println(no);
+		no++;
+	}
+
+}
+
